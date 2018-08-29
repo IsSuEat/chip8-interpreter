ea96e2ff8eab9e0dd3fb511c8f7cf417df2c7b3f
armin <issue@archlinux.info>
2018-08-29 20:39:57 +0200

    Implement more opcodes

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::Bytes;
+use std::num::Wrapping;
 
 const FONTSET: [u8; 80] = [
     0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
@@ -106,7 +107,10 @@ impl Cpu {
             0x1000 => self.op_1nnn(),
             0x2000 => self.op_2nnn(),
             0x3000 => self.op_3xnn(),
+            0x4000 => self.op_4xnn(),
+            0x5000 => self.op_5xy0(),
             0x6000 => self.op_6xnn(),
+            0x7000 => self.op_7xnn(),
             0x0000 => match opcode & 0x000F {
                 0x0000 => self.op_00e0(),
                 0x000E => self.op_00ee(),
@@ -117,7 +121,11 @@ impl Cpu {
     }
 
     fn op_unknown(&mut self) {
-        println!("Unknown opcode {:X}", self.opcode);
+        println!(
+            "Unknown opcode 0x{:X}, 0x{:X}",
+            self.opcode,
+            self.opcode & 0xF000
+        );
         panic!();
     }
 
@@ -158,8 +166,8 @@ impl Cpu {
     /// Skips the next instruction if VX equals NN. (Usually the next instruction is a jump to skip a code block)
     /// pseudo c `if(Vx==NN)`
     fn op_3xnn(&mut self) {
-        let x = (self.opcode & 0x0F00) >> 8;
-        let nn = (self.opcode & 0x00FF) as u8;
+        let x = self._x();
+        let nn = self._nn();
 
         if self.v[x as usize] == nn {
             self.pc += 4;
@@ -168,19 +176,83 @@ impl Cpu {
         self.pc += 2;
     }
 
+    /// Skips the next instruction if VX doesn't equal NN. (Usually the next instruction is a jump to skip a code block)
+    /// pseudo c `if(Vx!=NN)`
+    fn op_4xnn(&mut self) {
+        let x = self._x();
+        let nn = self._nn();
+
+        if self.v[x as usize] != nn {
+            self.pc += 4;
+            return;
+        }
+        self.pc += 2;
+    }
+
+    /// Skips the next instruction if VX equals VY. (Usually the next instruction is a jump to skip a code block)
+    /// pseudo c `if(Vx==Vy)`
+    fn op_5xy0(&mut self) {
+        let x = self._x();
+        let y = self._y();
+        if self.v[x as usize] == self.v[y as usize] {
+            self.pc += 4;
+            return;
+        }
+        self.pc += 2;
+    }
+
     /// Sets VX to NN.
     fn op_6xnn(&mut self) {
-        let x = (self.opcode & 0x0F00) >> 8;
+        let x = self._x();
         let nn = (self.opcode & 0x00FF) as u8;
         println!("Setting V{} to {}", x, nn);
         self.v[x as usize] = nn;
         self.pc += 2;
+    }
+
+    /// Adds NN to VX. (Carry flag is not changed)
+    /// pseudo c `Vx += NN `
+    fn op_7xnn(&mut self) {
+        let x = self._x();
+        let nn = self._nn();
+
+        self.v[x as usize] = self.v[x as usize].wrapping_add(nn);
+    }
+
+    /// Extract X from opcode
+    fn _x(&self) -> u8 {
+        return ((self.opcode & 0x0F00) >> 8) as u8;
+    }
+
+    /// Extract Y from opcode
+    fn _y(&self) -> u8 {
+        return ((self.opcode & 0x00F0) >> 4) as u8;
+    }
+
+    /// Extract NN from opcode
+    fn _nn(&self) -> u8 {
+        return (self.opcode & 0x00FF) as u8;
     }
 }
 
 #[cfg(test)]
 mod tests {
     use cpu::*;
+
+    #[test]
+    fn test_x_y() {
+        let mut cpu = Cpu::default().init();
+        cpu.opcode = 0xDEAD;
+        assert_eq!(cpu._x(), 0xE);
+        assert_eq!(cpu._y(), 0xA);
+    }
+
+    #[test]
+    fn test_nn() {
+        let mut cpu = Cpu::default().init();
+        cpu.opcode = 0xDEAD;
+        assert_eq!(cpu._nn(), 0xAD);
+    }
 
     #[test]
     fn test_annn() {
@@ -204,7 +276,7 @@ mod tests {
     #[test]
     fn test_00ee() {
         let mut cpu = Cpu::default().init();
-        cpu.execute_opcode(0x0123);
+        cpu.execute_opcode(0x00EE);
     }
 
     #[test]
@@ -218,13 +290,6 @@ mod tests {
         let mut cpu = Cpu::default().init();
         cpu.execute_opcode(0x2123);
         assert_eq!(cpu.pc, 0x123);
-    }
-
-    #[test]
-    fn test_6xnn() {
-        let mut cpu = Cpu::default().init();
-        cpu.execute_opcode(0x62FF);
-        assert_eq!(cpu.v[2], 0xFF);
     }
 
     #[test]
@@ -244,4 +309,54 @@ mod tests {
         cpu.execute_opcode(0x32FF);
         assert_eq!(cpu.pc, 0x202);
     }
+
+    #[test]
+    fn test_4xnn() {
+        let mut cpu = Cpu::default().init();
+        // set register 2 to 0xFF
+        cpu.v[2] = 0xFF;
+        // if v[2] != 0xFF
+        cpu.execute_opcode(0x42FD);
+        // skip 4, starting at 0x200
+        assert_eq!(cpu.pc, 0x204);
+        // set register 2 to 0xF0
+        // if v[2[ == 0xFF only increment by 2
+        cpu.v[2] = 0xF0;
+        // reset pc
+        cpu.pc = 0x200;
+        cpu.execute_opcode(0x42F0);
+        assert_eq!(cpu.pc, 0x202);
+    }
+
+    #[test]
+    fn test_5xy0() {
+        let mut cpu = Cpu::default().init();
+        cpu.v[5] = 0xFF;
+        cpu.v[6] = 0xFF;
+        cpu.execute_opcode(0x5560);
+        assert_eq!(cpu.pc, 0x204);
+    }
+
+    #[test]
+    fn test_6xnn() {
+        let mut cpu = Cpu::default().init();
+        cpu.execute_opcode(0x62FF);
+        assert_eq!(cpu.v[2], 0xFF);
+    }
+
+    #[test]
+    fn test_7xnn() {
+        let mut cpu = Cpu::default().init();
+        cpu.execute_opcode(0x72FF);
+        assert_eq!(cpu.v[2], 0xFF);
+    }
+
+    #[test]
+    fn test_7xnn_overflow() {
+        let mut cpu = Cpu::default().init();
+        cpu.v[2] = 0xFF;
+        cpu.execute_opcode(0x7205);
+        assert_eq!(cpu.v[2], 0x04);
+    }
+
 }
