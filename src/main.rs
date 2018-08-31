e0b0b8ec29481e611a2d675f58952acbeeff0f04
armin <issue@archlinux.info>
2018-08-31 22:15:03 +0200

    Read rompath from args

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
 extern crate chip8;
 extern crate env_logger;
+
 use chip8::Chip8;
 
 use std::env;
+use std::time::Duration;
 
 fn main() {
     env_logger::init();
     let args: Vec<String> = env::args().collect();
 
-    //    if args.len() < 2 {
-    //        println!("no path provided");
-    //        return;
-    //    }
-    let rom_path = String::from("roms/sctest.c8");
+    let mut rom_path = &String::from("roms/sctest.c8");
+    if args.len() == 2 {
+        rom_path = &args[1];
+    }
 
     let mut c8 = Chip8::new();
-    c8.load_rom(&rom_path);
+    c8.load_rom(rom_path);
     c8.run();
 }
