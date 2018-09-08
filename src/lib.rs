b72b2dbf49222ce7df5a709b4ebacda93a6b1c2b
armin <issue@archlinux.info>
2018-09-08 16:00:25 +0200

    Remove not needed lib.rs

--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-#[macro_use]
-extern crate log;
-extern crate env_logger;
-extern crate piston_window;
-extern crate rand;
-
-pub mod chip8;
-pub mod cpu;
-pub use chip8::Chip8;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-extern crate chip8;
+#[macro_use]
+extern crate log;
 extern crate env_logger;
+extern crate piston_window;
+extern crate rand;
 
+pub mod chip8;
+pub mod cpu;
 use chip8::Chip8;
 
 use std::env;
